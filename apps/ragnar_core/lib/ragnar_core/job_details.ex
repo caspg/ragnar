dfce42ea4a6157333d25af6b74584257cccc1b1a
Kacper Golinski <kacper.golinski@gmail.com>
2017-09-11 19:42:50 +0200

    update job_details module

--- apps/ragnar_core/lib/ragnar_core/job_details.ex
+++ apps/ragnar_core/lib/ragnar_core/job_details.ex
@@ -3,8 +3,6 @@ defmodule RagnarCore.JobDetails do
   defstruct [:title, :origin_url, :publication_date, :client]
 
   @we_work_remotely_client_name "we_work_remotely"
-  @available_clients [@we_work_remotely_client_name]
 
   def we_work_remotely_client_name, do: @we_work_remotely_client_name
-  def available_clients, do: @available_clients
 end
--- apps/ragnar_core/test/ragnar_core/job_details_test.exs
+++ apps/ragnar_core/test/ragnar_core/job_details_test.exs
@@ -8,8 +8,4 @@ defmodule RagnarCoreTest.Client.JobDetailsTest do
   test "we_work_remotely_client_name" do
     assert JobDetails.we_work_remotely_client_name === @we_work_remotely
   end
-
-  test "available_clients" do
-    assert JobDetails.available_clients === [@we_work_remotely]
-  end
 end
