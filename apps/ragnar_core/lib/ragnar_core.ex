15c2f675b8e04e28bfd2834e57c5a30d2ad3960e
Kacper Golinski <kacper.golinski@gmail.com>
2017-09-13 22:59:03 +0200

    refactor RagnarCore.WeWorkRemotely module

--- apps/ragnar_core/lib/ragnar_core.ex
+++ apps/ragnar_core/lib/ragnar_core.ex
@@ -1,9 +1,3 @@
 defmodule RagnarCore do
   alias RagnarCore.WeWorkRemotely
-
-  def find_jobs(days_limit \\ nil) do
-    %{
-      we_work_remotely: WeWorkRemotely.find_jobs(days_limit),
-    }
-  end
 end
--- apps/ragnar_core/lib/ragnar_core/we_work_remotely.ex
+++ apps/ragnar_core/lib/ragnar_core/we_work_remotely.ex
@@ -1,20 +1,44 @@
 defmodule RagnarCore.WeWorkRemotely do
   @moduledoc """
-  Module responsible for fetching and parsing jobs
+  Module responsible for fetching, parsing and searching jobs
   from https://weworkremotely.com.
   """
 
+  alias RagnarCore.JobsSearch
   alias RagnarCore.Helper
   alias RagnarCore.WeWorkRemotely.Client
   alias RagnarCore.WeWorkRemotely.Parser
 
-  def find_jobs(nil) do
+  @doc """
+  Returns jobs matching terms.
+
+  ## Options
+
+    * `:contain_all?` (boolean) - if true, all terms must be included.
+      If false, just single term must be included.
+
+    * `:days_limit` (positive integer) - specify how old job add could be.
+
+  """
+  @spec find_jobs([String.t], keyword) :: [%RagnarCore.JobDetails{}]
+
+  def find_jobs(terms, options \\ []) do
+    contain_all? = Keyword.get(options, :contain_all?, false)
+    days_limit   = Keyword.get(options, :days_limit, nil)
+
+    fetch_and_parse_jobs
+    |> maybe_filter_jobs_by_date(days_limit)
+    |> JobsSearch.search_terms(terms, [contain_all?: contain_all?])
+  end
+
+  defp fetch_and_parse_jobs do
     rss_feed = Client.fetch_jobs_rss()
     Parser.parse_rss_feed(rss_feed)
   end
 
-  def find_jobs(days_limit) do
-    find_jobs(nil)
+  defp maybe_filter_jobs_by_date(jobs, nil), do: jobs
+  defp maybe_filter_jobs_by_date(jobs, days_limit) do
+    jobs
     |> Enum.filter(&job_is_not_old?(&1, days_limit))
   end
 
