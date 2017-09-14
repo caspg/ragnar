dbbfd47ccb5900d275eb99b6e1c1e5e3db40d0e9
Kacper Golinski <kacper.golinski@gmail.com>
2017-09-14 21:13:57 +0200

    update JobSearch

--- apps/ragnar_core/lib/ragnar_core/jobs_search.ex
+++ apps/ragnar_core/lib/ragnar_core/jobs_search.ex
@@ -8,31 +8,33 @@ defmodule RagnarCore.JobsSearch do
   alias RagnarCore.StringHelper
 
   @type jobs :: [%JobDetails{}]
-  @spec search_terms(jobs, [String.t], [contain_all?: boolean]) :: jobs
+  @spec search_terms(jobs, [String.t], boolean) :: jobs
 
   @doc """
   Returns jobs that contains terms.
 
   Looks for job that contains all terms or any, this behaviour can
   be specified with `contain_all?` keword.
+
   Terms are case insensitive.
   """
-  def search_terms(jobs, terms, contain_all?: contain_all?) do
+  def search_terms(jobs, terms, contain_all?) do
     jobs
     |> Enum.filter(&job_contains_terms?(&1, terms, contain_all?))
   end
 
-  defp job_contains_terms?(job_details, terms, false) do
+  defp job_contains_terms?(job_details, terms, contain_all?) do
     %JobDetails{title: title, description: description} = job_details
+    search_string = title <> " " <> description
 
-    StringHelper.string_contains_any_term?(title, terms, []) ||
-      StringHelper.string_contains_any_term?(description, terms, [])
+    search_function(search_string, terms, contain_all?)
   end
 
-  defp job_contains_terms?(job_details, terms, true) do
-    %JobDetails{title: title, description: description} = job_details
+  defp search_function(string, terms, true) do
+    StringHelper.string_contains_all_terms?(string, terms)
+  end
 
-    StringHelper.string_contains_all_terms?(title, terms, []) ||
-      StringHelper.string_contains_all_terms?(description, terms, [])
+  defp search_function(string, terms, false) do
+    StringHelper.string_contains_any_term?(string, terms)
   end
 end
